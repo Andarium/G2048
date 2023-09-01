540570802a8d34326e8533919145eb5c53015fe0
Andarium <voronmaks@herocraft.com>
2023-09-01 15:46:22 +0700

    Add entry point to play the game

---
+++ G2048_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using G2048;
+
+namespace G2048_Console;
+
+public abstract class Program
+{
+    private static void Main()
+    {
+        var random = new ConsoleRandom();
+        var input = new ConsoleGameInput();
+        var output = new ConsoleGameOutput(Game.BoardSize);
+        var score = new LocalScore();
+        var game = new Game(input, output, score, random);
+
+        while (true)
+        {
+            game.Update();
+        }
+    }
+}
