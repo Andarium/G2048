4433a110c505601702b3f933f69a7ead4538123a
Andarium <voronmaks@herocraft.com>
2023-09-01 15:45:03 +0700

    Implement ConsoleGameInput

---
+++ G2048_Console/Core/CommandBinding.cs
@@ -0,0 +1,18 @@
+﻿using G2048;
+
+namespace G2048_Console;
+
+public sealed class CommandBinding
+{
+    public readonly GameCommandType Type;
+    public readonly IReadOnlyList<ConsoleKey> Keys;
+
+    public CommandBinding(GameCommandType type, params ConsoleKey[] keys)
+    {
+        Type = type;
+        Keys = keys
+            .Distinct()
+            .OrderBy(x => x)
+            .ToList();
+    }
+}
---
+++ G2048_Console/Core/ConsoleGameInput.cs
@@ -0,0 +1,35 @@
+﻿using G2048;
+
+namespace G2048_Console;
+
+public sealed class ConsoleGameInput : IGameInput
+{
+    public GameCommandType ReadCommand()
+    {
+        var key = Console.ReadKey(true);
+
+        if (CommandsByKey.TryGetValue(key.Key, out var command))
+        {
+            return command;
+        }
+
+        return GameCommandType.None;
+    }
+
+    private static readonly List<CommandBinding> Commands = new()
+    {
+        new(GameCommandType.Quit, ConsoleKey.Q),
+        new(GameCommandType.MoveLeft, ConsoleKey.A, ConsoleKey.LeftArrow),
+        new(GameCommandType.MoveRight, ConsoleKey.D, ConsoleKey.RightArrow),
+        new(GameCommandType.MoveUp, ConsoleKey.W, ConsoleKey.UpArrow),
+        new(GameCommandType.MoveDown, ConsoleKey.S, ConsoleKey.DownArrow),
+        new(GameCommandType.Restart, ConsoleKey.R),
+        new(GameCommandType.Confirm, ConsoleKey.Y),
+        new(GameCommandType.Cancel, ConsoleKey.N)
+    };
+
+    private static readonly Dictionary<ConsoleKey, GameCommandType> CommandsByKey =
+        Commands
+            .SelectMany(x => x.Keys.Select(y => new { Key = y, Command = x.Type }))
+            .ToDictionary(x => x.Key, y => y.Command);
+}
